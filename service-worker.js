8a03e9048289f052fd58cf2bfc4c4f512e3c4da4
bayyild <bayyild@gmail.com>
2025-10-12 13:49:15 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,29 +9,32 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
+  console.log("Service Worker: Installing & caching files...");
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
+  console.log("Service Worker: Activated");
   event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => {
-        if (
-          event.request.mode === "navigate" ||
-          event.request.headers.get("accept").includes("text/html")
-        ) {
-          return caches.match("./index.html");
-        }
-      });
+      return (
+        response ||
+        fetch(event.request).catch(() => {
+          if (
+            event.request.mode === "navigate" ||
+            event.request.headers.get("accept")?.includes("text/html")
+          ) {
+            return caches.match("./index.html");
+          }
+        })
+      );
     })
   );
 });
